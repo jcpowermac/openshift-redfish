b5e61f8da433f88ab70d654fe0e5ff5e5ee55ccc
Joseph Callen <jcallen@redhat.com>
2023-07-25 14:39:38 -0400

    move scripts, variables

---
+++ variables.ps1
@@ -0,0 +1,13 @@
+$bmc = @"
+{
+    "m0": {
+        "secret": "/var/run/secret/vcenter/ibm.xml"
+    },
+    "m1": {
+        "secret": "/var/run/secret/vcenter/ibm8.xml"
+    },
+    "m2": {
+        "secret": "/var/run/secret/vcenter/devqe.xml"
+    }
+}
+"@
