b4b82eed9e6ae96195cec4d58cfaa8c7dcebe927
Joseph Callen <jcallen@redhat.com>
2023-07-26 15:28:17 -0400

    add slack

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -7,26 +7,10 @@ Set-PowerCLIConfiguration -InvalidCertificateAction:Ignore -Confirm:$false | Out
 $bmchash = ConvertFrom-Json -InputObject $bmc -AsHashtable
 $slackMessage = @"
 BMC: {0}
-Host: {1}
+Node: {1}
+Severity {2}
+Message: {3}
 "@
-
-# Id                             2833
-# @odata.context                 /redfish/v1/$metadata#LogEntry.LogEntry
-# @odata.type                    #LogEntry.v1_11_0.LogEntry
-# @odata.id                      /redfish/v1/Systems/1/LogServices/AuditLog/Entries/2833
-# EventId                        0x400000a200000000
-# Oem                            {[Lenovo, @{EventType=0; ReportingChain=XCC; IsLocalEvent=True; Ra…
-# @odata.etag                    "ejUxX1N0YW5kYXJkTG9nRW50cnkK2833"
-# EntryType                      Oem
-# Name                           LogEntry
-# OemRecordFormat                Lenovo
-# Description                    This resource is used to represent a log entry for log services fo…
-# Created                        7/20/2023 3:35:54 PM
-# Severity                       OK
-# Message                        User LXPM has mounted file tdm_image.img from Local.
-# OemLogEntryCode                Lenovo0079
-# MessageArgs                    {IPMIMBOX, 07/20/2023, 15:37:11, false…}
-# EventGroupId                   0
 
 
 $last24h = (Get-Date).AddHours(-24)
@@ -54,6 +38,7 @@ foreach ($key in $bmchash.Keys) {
                     if(-not $l.Severity.ToLower().Contains("ok")) {
                         $l.Message
                         $l.Severity
+                        Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $bmchash[$key].ip, $bmchash[$key].node, $l.Severity, $l.Message)
                     }
                 }
             }
@@ -61,7 +46,6 @@ foreach ($key in $bmchash.Keys) {
             }
         }
 
-        #Send-SlackMessage -Uri $Env:SLACK_WEBHOOK_URI -Text ($slackMessage -f $cihash[$key].vcenter)
     }
     catch {
         Get-Error
